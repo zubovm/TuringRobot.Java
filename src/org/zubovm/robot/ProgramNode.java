765a80c89e8ca5fdc46a65861ce2af5c1b3b66e0
Michael Zubov <zubovm@gmail.com>
2016-08-02 02:36:53 +0300

    Children in ProgramNode appears

--- src/org/zubovm/robot/ProgramNode.java
+++ src/org/zubovm/robot/ProgramNode.java
@@ -1,17 +1,21 @@
 package org.zubovm.robot;
 
+import org.zubovm.robot.test.EmptyCommandNode;
+
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by michael on 02.08.16.
  */
 public class ProgramNode implements RobotDocumentNode {
     private final String name;
-    private final LinkedList<RobotDocumentNode> children;
+    private final List<RobotDocumentNode> children;
 
     public ProgramNode(String programName) {
         this.name = programName;
-        this.children = new LinkedList<RobotDocumentNode>();
+        this.children = Arrays.asList(new EmptyCommandNode());
     }
 
     @Override
@@ -21,6 +25,6 @@ public class ProgramNode implements RobotDocumentNode {
 
     @Override
     public RobotDocumentNode stepIn() {
-        return null;
+        return children.get(0);
     }
 }
